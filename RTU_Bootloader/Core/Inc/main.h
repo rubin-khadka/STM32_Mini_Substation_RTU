8968b57a0608acdb6552b2f39829b1bfa2bb4292
Rubin Khadka <rubinkhadka84@gmail.com>
2026-08-15 17:29:05 +0200

    config 100 Mhz and spi for external flash

--- RTU_Bootloader/Core/Inc/main.h
+++ RTU_Bootloader/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define SS_Pin GPIO_PIN_12
+#define SS_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
