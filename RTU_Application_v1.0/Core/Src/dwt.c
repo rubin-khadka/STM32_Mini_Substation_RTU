3646da4c62f18e5b62cdcce37509bdca3dadd7d2
Rubin Khadka <rubinkhadka84@gmail.com>
2026-08-24 15:47:01 +0200

    using dwt for microsecond delay only

--- RTU_Application_v1.0/Core/Src/dwt.c
+++ RTU_Application_v1.0/Core/Src/dwt.c
@@ -8,11 +8,6 @@
 #include "dwt.h"
 #include "main.h"
 
-// DWT cycle counter
-#define DWT_CYCCNT_R  (DWT->CYCCNT)
-
-static volatile uint32_t tick_counter = 0;
-
 // Initialize DWT cycle counter
 void DWT_Init(void) {
 	// Enable trace and debug
@@ -23,27 +18,11 @@ void DWT_Init(void) {
 
 	// Enable cycle counter
 	DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
-
-	tick_counter = 0; // Initialize Counter
 }
 
-// Get current tick count (milliseconds)
-uint32_t DWT_GetTick(void) {
-	return tick_counter + (DWT_CYCCNT_R / (SystemCoreClock / 1000));
-}
-
-// Delay for microseconds using DWT
-void DWT_Delay_us(uint32_t us)
-{
-  uint32_t cycles = (uint32_t)(((uint64_t)SystemCoreClock * us) / 1000000);
-  uint32_t start = DWT->CYCCNT;
-  while ((DWT->CYCCNT - start) < cycles);
-}
-
-// Delay for milliseconds using DWT
-void DWT_Delay_ms(uint32_t ms) {
-	while (ms--) {
-		DWT_Delay_us(1000);
-		tick_counter++;
-	}
+// Microsecond delay using DWT
+void DWT_Delay_us(uint32_t us) {
+    uint32_t cycles = (uint32_t)(((uint64_t)SystemCoreClock * us) / 1000000);
+    uint32_t start = DWT->CYCCNT;
+    while ((DWT->CYCCNT - start) < cycles);
 }
